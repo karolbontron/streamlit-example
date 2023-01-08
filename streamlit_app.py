0169c74e2be57196c8c39e47d0496d670720a0f2
karolbontron <karol.bontron@protonmail.ch>
2023-01-08 21:29:26 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,32 +1,11 @@
-import streamlit as st
-import pandas as pd
 import numpy as np
-import plotly.express as px
 import plotly.graph_objects as go
-import requests
-import json
-import datetime
-import time
-import os
+import streamlit as st
 
-# Get data from API
-def get_data(url):
-    response = requests.get(url)
-    data = response.json()
-    return data
+z = np.random.random_sample((3, 2))
+fig = go.Figure(data=go.Contour(z=z))
 
-# Get data from API and convert to dataframe
-def get_data_to_df(url):
-    data = get_data(url)
-    df = pd.DataFrame(data)
-    return df
-
-# Get data from API and convert to dataframe
-def get_data_to_df_with_pagination(url, page_size, page_number):
-    url = url + '?pageSize=' + str(page_size) + '&pageNumber=' + str(page_number)
-    data = get_data(url)
-    df = pd.DataFrame(data)
-    return df
-
-# Get data from API and convert to dataframe
-def get_data_to
+st.plotly_chart(
+    fig, 
+    theme="streamlit",  # ✨ Optional, this is already set by default!
+)
